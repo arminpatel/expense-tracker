270aea5c78ccd378e3a66ee10d9c85fcca9d03d0
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-04-04 03:33:22 +0530

    initialize project

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Armin Patel on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
