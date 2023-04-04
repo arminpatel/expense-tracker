7b680c304027e9fe22ad0af126b0105402bab89c
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-04-04 22:25:42 +0530

    add data model for expenses

---
+++ Expense+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Expense+CoreDataClass.swift
+//  Expense Tracker
+//
+//  Created by Armin Patel on 04/04/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Expense)
+public class Expense: NSManagedObject {
+
+}
